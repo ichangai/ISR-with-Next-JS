dc464f538b44de91beac3a82f2b5dfda44f7f33b
Ichangai <ichangaigitundu@gmail.com>
2022-05-23 17:09:23 +0300

    ISR in Next-JS

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+
+function Home() {
+    return (
+        <>
+            <h1>Next JS pre-rendering</h1>
+            <Link href="/users">
+                <button>Users</button>
+            </Link>
+            <Link href="/posts">
+                <button>Posts</button>
+            </Link>
+            <Link href="/products">
+                <button>Products</button>
+            </Link>
+        </>
+    )
+}
+
+export default Home
